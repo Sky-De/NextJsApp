dca2344f785aee06756196aa82ad86d7e5d9f0b7
sky1991 <sky.dfo@gmail.com>
2022-04-19 14:32:04 +0430

    routersEnd

---
+++ pages/docs/[[...params]].js
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router';
+const DocHome = () => {
+    const router = useRouter();
+    const { params=[] } = router.query;
+    console.log(params);
+  return (
+    <div>DocHome </div>
+  )
+}
+
+export default DocHome
